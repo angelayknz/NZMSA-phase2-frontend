a1b06b36b0b8f7106b7844571c86800ee33ec412
Angela Yang <kyan211@mywhitecliffe.com>
2022-08-02 22:05:59 +1200

    complete step 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import IconButton from '@mui/material/IconButton'
+import SearchIcon from '@mui/icons-material/Search'
+import TextField from '@mui/material/TextField'
+import { Box, Button, Grid, Paper, Skeleton } from '@mui/material'
 
 function App() {
+  // Declare a new state variable, which we'll call "pokemonName"
+  // const [pokemonName, setPokemonName] = useState('')
+  const [pokemonID, setPokemonID] = useState('')
+  const [pokemonInfo, setPokemonInfo] = useState<undefined | any>(undefined)
+
+  const POKEMON_BASE_URL = 'https://api.disneyapi.dev/characters'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Disney Charactor Search</h1>
+
+      <div>
+        {/* <label>Enter the Charactor ID</label> */}
+        <br />
+        {/* <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={(e) => setPokemonID(e.target.value)}
+        /> */}
+
+        <TextField
+          id="search-bar"
+          className="text"
+          value={pokemonID}
+          onChange={(prop: any) => {
+            setPokemonID(prop.target.value)
+          }}
+          label="Enter a charactor ID..."
+          variant="outlined"
+          placeholder="Search..."
+          size="small"
+        />
+
+        <IconButton
+          aria-label="search"
+          onClick={() => {
+            search()
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <SearchIcon style={{ fill: 'blue' }} />
+        </IconButton>
+
+        <br />
+        {/* <button onClick={search}>Search</button> */}
+      </div>
+
+      <p>You have entered {pokemonID}</p>
+
+      {pokemonInfo === undefined ? (
+        <p>Charactor not found. Please try again.</p>
+      ) : (
+        <div id="pokemon-result">
+          {pokemonInfo.imageUrl === null || pokemonInfo === null ? (
+            <p>No image found</p>
+          ) : (
+            <img src={pokemonInfo.imageUrl} />
+          )}
+          <p>
+            Name: {pokemonInfo.name}
+            <br />
+            TV Shows: {pokemonInfo.tvShows}
+          </p>
+        </div>
+      )}
     </div>
-  );
+  )
+
+  function search() {
+    if (pokemonID === undefined || pokemonID === '') {
+      return
+    }
+
+    axios
+      .get(POKEMON_BASE_URL + '/' + pokemonID)
+      .then((res) => {
+        // console.log(res.data)
+        setPokemonInfo(res.data)
+      })
+      .catch((err) => {
+        console.log('Charactor not found')
+        setPokemonInfo(undefined)
+      })
+  }
 }
 
-export default App;
+export default App
